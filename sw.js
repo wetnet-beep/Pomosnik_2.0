04c0c068be173672ba610318959a2fabbab2c88b
wetnet-beep <avalbaevamirhan3@gmail.com>
2025-11-30 11:25:16 +0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,28 @@
+// sw.js - Service Worker для оффлайн работы
+const CACHE_NAME = 'school-helper-v1';
+const urlsToCache = [
+  '/',
+  '/index.html'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
